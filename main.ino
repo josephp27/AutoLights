5a36fa681ad505cef4dbcd1d36edc40337917833
Joseph Procopio <josephp27@users.noreply.github.com>
2021-03-14 13:43:17 -0500

    Uses BLE

--- main.ino
+++ main.ino
@@ -1,27 +1,64 @@
-int led1 = D6;
+// We don't want to connect to the cloud
+SYSTEM_MODE(MANUAL);
+
+// UUIDs for service + characteristics
+const char *serviceUuid = "b4250400-fb4b-4746-b2b0-93f0e61122c6"; //service
+const char *ledUuid = "b4250401-fb4b-4746-b2b0-93f0e61122c6";     //led
 bool is_on = false;
 
-void setup() {
-    // turn off the main led to save battery
-    RGB.brightness(0);
-    
-    pinMode(led1, OUTPUT);
-    Particle.function("led", ledToggle);
-}
+// Static function for handling Bluetooth Low Energy callbacks
+static void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context)
+{
 
-void loop() {
-    //empty for now
-}
+  // We're only looking for one byte
+  if (len != 1)
+  {
+    return;
+  }
 
-
-int ledToggle(String command) {
+  if (data[0] == 0xFF)
+  {
+    is_on = true;
+  }
+  else if (data[0] == 0x00)
+  {
+    is_on = false;
+  }
+  else
+  {
     is_on ^= 1;
-    
-    if (is_on == true) {
-        digitalWrite(led1, HIGH);
-        return 1;
-    }
+  }
 
-    digitalWrite(led1, LOW);
-    return 0;
+  digitalWrite(D6, is_on);
+}
+
+// turn off wifi and set led pin
+void setup()
+{
+  WiFi.off();
+  pinMode(D6, OUTPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+  // Set up characteristics
+  BleCharacteristic ledCharacteristic("led", BleCharacteristicProperty::WRITE_WO_RSP, ledUuid, serviceUuid, onDataReceived, (void *)ledUuid);
+  
+  // Add the characteristics
+  BLE.addCharacteristic(ledCharacteristic);
+
+  // Advertising data
+  BleAdvertisingData advData;
+
+  // Start advertising!
+  BLE.advertise(&advData);
+
+  delay(10000);
+
+  SystemSleepConfiguration config;
+  config.mode(SystemSleepMode::ULTRA_LOW_POWER)
+      .ble();
+
+  SystemSleepResult res = System.sleep(config);
 }
